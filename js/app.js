d6357a31323f5aca0acd78f4b7b7431c685bedf2
aperkins <andy.perkins@rufusleonard.com>
2015-09-17 16:41:34 +0100

    initial scaffold

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var app = 
+{
+    dotNetDays : 0,
+    frontendDays : 0,
+    qaDays : 0,
+
+
+    init: function () 
+    {
+
+        window.addEventListener('resize', app.onWindowResize, false);
+
+        $("#sphereButton").click(function () 
+        {
+            
+        });
+    },
+
+    onWindowResize : function () 
+    {
+  
+    }
+};
+
+function trixelate( mesh ) 
+{
+	//var trixel = new Trixel( tm, [ position ] ) ;
+};
+
+function Trixel( mesh, positions ) 
+{
+
+};
+
+}
