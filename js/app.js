ef37d3e02946511fbca0fc61e7939c4e81ec42db
aperkins <andy.perkins@rufusleonard.com>
2015-09-17 18:17:59 +0100

    updated fields

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
 var app = 
 {
-    dotNetDays : 0,
-    frontendDays : 0,
+    beDays : 0,
+    feDays : 0,
     qaDays : 0,
-
+    iterations : 0,
+    iterationDuration : 0,
+    feTeamSize : 0,
+    beTeamSize : 0,
+    feManagementPercentage : 0,
+    backendManagementPercentage : 0,
+    iteration1Efficiency : 0,
+    iternationNEfficiency : 0,
+    QAPercentageEffort : 0,
+    DCPercentageBuildTime : 0,
+    TechLeadershipTime : 0,
+    UATSupportPercent : 0,
+    WarrantySupportPercent : 0,
 
     init: function () 
     {
 
         window.addEventListener('resize', app.onWindowResize, false);
 
-        $("#sphereButton").click(function () 
+        /*$("#dotNetDays").keyup(function()
         {
-            
-        });
+    		var value = $( this ).val();
+  		})
+  		.keyup();
+		*/
+
+  		$("#submitBtn").click(function()
+  		{
+  			app.feDays = $("#frontendDays").val();
+  			app.beDays = $("#backendDays").val();
+
+  			setText();
+  		});
+  			
     },
 
     onWindowResize : function () 
@@ -22,14 +45,14 @@ var app =
     }
 };
 
-function trixelate( mesh ) 
+function setText(  ) 
 {
 	//var trixel = new Trixel( tm, [ position ] ) ;
+	//console.log(app.feDays + "," + app.beDays);
+	$( "p" ).text( app.feDays + "," + app.beDays );
 };
 
 function Trixel( mesh, positions ) 
 {
 
 };
-
-}
